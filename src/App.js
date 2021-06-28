a6d526bfc336c84632f39d70b6bdc3f9935d4f06
hrushi-thorat <hrushikeshst54@gmail.com>
2021-06-27 23:58:39 -0700

    movie setion

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutUs from "./pages/AboutUs";
 import ContactUS from "./pages/ContactUS";
 import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/movieDetails";
 // Import Styles
 import GlobalStyle from "./components/GlobalStyleComponents";
 // Import React Router
@@ -17,8 +18,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUS />
