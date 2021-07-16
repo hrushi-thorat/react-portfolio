709105876a70145cbcbe885c2de4019cd86b00f0
hrushi-thorat <hrushikeshst54@gmail.com>
2021-07-16 05:02:04 -0700

    fixing issues

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -6,6 +6,7 @@ import js from "../img/js.svg"
 import figma from "../img/figma.svg"
 import sass from "../img/sass.svg"
 import react from "../img/react.svg"
+import java from "../img/java-icon.svg"
 
 
 // import Styles
@@ -17,7 +18,7 @@ const ServicesSection = () => {
     <Skills>
       <Heading>
       <h2>My Skills
-        <div variants={lineAnim} className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
       </h2>
 
     <h3>Front-end</h3>
@@ -42,10 +43,15 @@ const ServicesSection = () => {
 <SkillCard>
 <img src={figma} alt="html" />
 </SkillCard>
-
       </SkillSet>
-
+<h3>Languages familar with
+<motion.div variants={lineAnim} className="line"></motion.div>
+</h3>
+<SkillCard>
+  <img src={java} alt="java" />
+</SkillCard>
       </SkillSetContainer>
+
     </Skills>
   );
 };
@@ -53,34 +59,49 @@ const Skills=styled(motion.div)`
 height: 100vh;
 display: flex;
 align-items: center;
-justify-content: space-between;
 flex-direction: column;
 margin-top:1rem;
 `
 const Heading=styled(motion.div)`
 text-align:center;
+
 h2{
   color: rgba(255,255,255,80%);
   font-size: 3rem;
   .line {
     height: 0.3rem;
     background: #23d992;
-    margin-bottom: 1rem;
+    margin-top: 0.5rem;
     width: 100%;
+    font-weight: 700;
   }
 }
 h3{
+  font-weight:lighter;
   font-size: 1.3rem;
   margin-top: 1rem;
   color: rgba(255,255,255,70%);
 }
 `
 const SkillSetContainer=styled(motion.div)`
-
-margin: auto;
+margin:1rem auto;
 display: flex;
 align-items: center;
 justify-content: center;
+flex-direction: column;
+h3{
+  .line {
+    height: 0.3rem;
+    background: #23d992;
+    margin-top: 0.5rem;
+    width: 100%;
+    font-weight: 700;
+  }
+  font-weight:lighter;
+  font-size: 1.8rem;
+  margin-top: 2rem;
+  color: rgba(255,255,255,70%);
+}
 `
 const SkillSet=styled(motion.div)`
 width: 50%;
@@ -90,8 +111,8 @@ flex-wrap: wrap;
 `
 
 const SkillCard=styled(motion.div)`
-height: 7.5rem;
-width: 7.5rem;
+height: 6.5rem;
+width: 6.5rem;
 border-radius: 50%;
 background: #C4C4C4;
 display: flex;
